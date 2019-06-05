4ac08ef86dc2738a3852d4c9c4ce19e06abfc5e5
RAMIRO <pfcperez@gmail.com>
2019-06-05 11:48:16 -0500

    New additions to comply

--- plural/functions.py
+++ plural/functions.py
@@ -1,59 +1,49 @@
 students = []
 
+
 def get_students_titlecase():
-    students_titlecase = []
-    for student in students:
-        students_titlecase.append(student["name"].title())
-    return students_titlecase
+	students_titlecase = []
+	for student in students:
+		students_titlecase.append(student["name"].title())
+	return students_titlecase
 
 
 def print_students_titlecase():
-    students_titlecase = get_students_titlecase()
-    print(students_titlecase)
+	students_titlecase = get_students_titlecase()
+	print(students_titlecase)
+
 
 def add_student(name, student_id=332):
-    student = {'name':name, 'student_id':student_id}
-    students.append(student)
+	student = {"name": name, "student_id": student_id}
+	students.append(student)
+
 
 def save_file(student):
-    try:
-        f = open("students.txt","a")
-        f.write(student + "\n")
-        f.close()
-    except Exception:
-        print("Could not save file")
+	try:
+		f = open("students.txt", "a")
+		f.write(student + "\n")
+		f.close()
+	except Exception:
+		print("Could not save file")
+
 
 def read_file():
-    try:
-        f = open("students.txt","r")
-        for student in read_students(f):
-            add_student(student)
-        f.close()
-    except Exception:
-        print("Could not read this file")
+	try:
+		f = open("students.txt", "r")
+		for student in f.readlines():
+			add_student(student)
+		f.close()
+	except Exception:
+		print("Could not read file")
 
-"""
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"])
-"""
-def read_students(f):
-    for line  in f:
-        yield line
 
 read_file()
-print(students)
+print_students_titlecase()
 
-
-""" 
 student_name = input("Enter student name: ")
-student_id = input("Enter student id: ")
+student_id = input("Enter student ID: ")
 
 add_student(student_name, student_id)
 save_file(student_name)
 
 
-print_students_titlecase()
-"""
-
-#var_args("Mark",  description ="Loves python", feedback=None, subscriber=True)
